f441858b6fe0b7369b77a8a994391a9510eda790
xGp35 <girijaprasanna1999@gmail.com>
2025-10-28 23:33:39 +0530

    post reqest give 500 error

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const app = require('express')();
 const PORT = 8080;
 
-app.listen(
-    PORT,
-    () => console.log(`Server is running on http://localhost:${PORT}`)
-)
+app.get('/boobies', (req, res) => {
+    res.status(200).send({
+        message: "Boobies are great!",
+        size: "large",
+        shape: "round",
+        representation: "🍊"
+    })
+    console.log("Boobies endpoint hit");
+});
 
+app.post('/boobies/:id', (req, res) => {
+
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if(!logo) {
+        res.status(418).send({message: 'I need a logo to proceed!'})
+    }
+
+    res.send({
+        message: `Boobies with id ${id} have been updated with logo ${logo}`
+    })
+
+});
+
+app.listen(
+    PORT, 
+    () => console.log(`Server is running on http://localhost:${PORT}`)
+);
